0901785b57067d655d6d1789ea246e35752f6988
random-debug <85326677+p-vinh@users.noreply.github.com>
2024-01-12 23:57:21 -0800

    help.

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('scheduleForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    const date = document.getElementById('date').value;
-    const time = document.getElementById('time').value;
-
-    const resultDiv = document.getElementById('scheduleResult');
-    resultDiv.innerHTML = `Appointment scheduled for ${date} at ${time}.`;
-});
