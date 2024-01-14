db09ef9daa677f8796b77fe5a3d007028d4502a0
CastroIROCZ <124112134+CastroIROCZ@users.noreply.github.com>
2024-01-13 19:05:57 -0800

    Update barcodeAPI.py

--- barcodeAPI.py
+++ barcodeAPI.py
@@ -4,49 +4,57 @@ from pyzbar.pyzbar import decode
 
 # Make one method to decode the barcode 
 def BarcodeReader(image):
-	
-	# read the image in numpy array using cv2
-	img = cv2.imread(image)
-	
-	# Decode the barcode image
-	detectedBarcodes = decode(img)
-	
-	# If not detected then print the message
-	if not detectedBarcodes:
-		print("Barcode Not Detected or your barcode is blank/corrupted!")
-	else:
-	
-		# Traverse through all the detected barcodes in image
-		for barcode in detectedBarcodes: 
-		
-			# Locate the barcode position in image
-			(x, y, w, h) = barcode.rect
-			
-			# Put the rectangle in image using 
-			# cv2 to highlight the barcode
-			cv2.rectangle(img, (x-10, y-10),
-						(x + w+10, y + h+10), 
-						(255, 0, 0), 2)
-			
-			if barcode.data!="":
-			
-			# Print the barcode data
-				print(barcode.data)
-				print(barcode.type)
+    
+    barcode_number = ''
+    
+    # read the image in numpy array using cv2
+    img = cv2.imread(image)
+    
+    # Decode the barcode image
+    detectedBarcodes = decode(img)
+    
+    # If not detected then print the message
+    if not detectedBarcodes:
+        print("Barcode Not Detected or your barcode is blank/corrupted!")
+    
+    else:
+        # Traverse through all the detected barcodes in image
+        for barcode in detectedBarcodes:
+            
+            # Locate the barcode position in image
+            (x, y, w, h) = barcode.rect
+            
+            # Put the rectangle in image using 
+            # cv2 to highlight the barcode
+            cv2.rectangle(img, (x-10, y-10),
+                          (x + w+10, y + h+10), 
+                          (255, 0, 0), 2)
+            
+            if barcode.data!="":
+                
+            # Print the barcode data
+             #print(barcode.data)
+             #print(barcode.type)
+             barcode_number = (barcode.data.decode('utf-8'))
+    
+    return barcode_number
+
 				
 	#Display the image
+'''
 	cv2.imshow("Image", img)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
+    '''
 
 if __name__ == "__main__":
-# Take the image from user
-	image="Img.jpg"
-	BarcodeReader(image)
+    # Take the image from user
+    image="Img.jpg"
+    barcode = BarcodeReader(image)
+    
 
 import requests
 
-barcode = '0888849003860'  #place holder where extracted barcode will go from the scanner 
 url = f'https://world.openfoodfacts.org/api/v0/product/{barcode}.json' #API endpoint
 
 response = requests.get(url)
